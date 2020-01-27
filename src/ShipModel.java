5c34d94dbe97e509312602b697a8708703fbbb5a
PhilouIPI <philippe.naovarath@campus-igs-toulouse.fr>
2020-01-27 15:44:02 +0100

    classe

---
+++ src/ShipModel.java
@@ -0,0 +1,11 @@
+
+public enum ShipModel {
+	Carrier(5),Battleship(4),Cruiser(3),Submarin(3),Destroyer(2);
+	
+private final int taille;
+	
+	private ShipModel(int taille){
+			this.taille = taille;		
+		}
+		
+}
