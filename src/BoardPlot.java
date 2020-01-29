a077b73a3239535a5311573ab8db1fc2a7d73240
PhilouIPI <philippe.naovarath@campus-igs-toulouse.fr>
2020-01-29 16:01:32 +0100

    classe5 add getPlotTo() in BoardPlot class

--- src/BoardPlot.java
+++ src/BoardPlot.java
@@ -3,6 +3,40 @@ public class BoardPlot implements Plot{
 	private int abscisse;
 	private int ordonnee;
 	
+	public BoardPlot(int abscisse, int ordonnee) {
+		this.abscisse = abscisse;
+		this.ordonnee = ordonnee;
+	}
+
+	@Override
+	public int getAbscisse() {
+		// TODO Auto-generated method stub
+		return this.abscisse;
+	}
+
+	@Override
+	public int getOrdonnee() {
+		// TODO Auto-generated method stub
+		return this.ordonnee;
+	}
+
+	@Override
+	public Plot getPlotTo(Direction d) {
+		// TODO Auto-generated method stub
+		switch(d) {
+		case NORTH : 
+			return new BoardPlot(this.abscisse+1, this.ordonnee);
+		case SOUTH : 
+			return new BoardPlot(this.abscisse-1, this.ordonnee);
+		case EAST :
+			return new BoardPlot(this.abscisse, this.ordonnee+1);
+		case WEST :
+			return new BoardPlot(this.abscisse, this.ordonnee-1);
+			default :
+				return new BoardPlot(0,0);
+				
+		}
+	}
 	
 
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,4 +1,26 @@
+import java.util.List;
 
 public class Ship {
+Plot coordonnee;
+ShipModel model;
+Direction direction;
 
+	List<Plot> listCases;
+
+	public Ship(Plot c, ShipModel m, Direction d) {
+		this.coordonnee = c;
+		this.model = m;
+		this.direction = d;
+		
+	}
+	public boolean isOnPlot(Plot p) {
+		if (this.listCases.contains(p)) {
+			return true;
+	} 
+		else { 
+			return false;
+		}
+	}
+	
 }
+
